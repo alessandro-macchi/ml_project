7a40e7d33198e371b846007fcef3eb5fb08bab8b
Alessandro Macchi <alessandro.macchi6@studenti.unimi.it>
2025-08-03 15:11:17 +0200

    Added utility functions in `utils.py` for consistent model naming and wine feature access.

--- src/utils.py
+++ src/utils.py
@@ -52,3 +52,25 @@ def create_named_kernels(gamma_values, degree_values, coef0_values):
             poly_kernels.append(named_kernel)
 
     return rbf_kernels + poly_kernels
+
+def get_model_names():
+    """
+    Get display names for all models
+
+    Returns:
+        dict: Mapping of model keys to display names
+    """
+    return {
+        'lr_custom': 'Logistic Regression',
+        'svm_custom': 'Linear SVM',
+        'klr_custom': 'Kernel Logistic Regression',
+        'ksvm_custom': 'Kernel SVM'
+    }
+
+def get_wine_feature_names():
+    """Get wine feature names for analysis"""
+    return [
+        'fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar',
+        'chlorides', 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density',
+        'pH', 'sulphates', 'alcohol', 'wine_type'
+    ]
