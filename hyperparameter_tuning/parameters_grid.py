bad05298e98200b3258a123d2a8125806ef2ffbd
Alessandro Macchi <alessandro.macchi6@studenti.unimi.it>
2025-08-27 17:48:17 +0200

    Refined hyperparameter grids in parameters_grid.py for cleaner configurations and improved readability.

--- hyperparameter_tuning/parameters_grid.py
+++ hyperparameter_tuning/parameters_grid.py
@@ -3,29 +3,30 @@ def get_parameter_grids():
     """
     Choose different values for different models' hyperparameters.
     To try different grids, just change the values here.
+    The commented values have been tried and excluded for computational time.
     """
     return {
         'logistic_regression': {
-            'learning_rate': [0.01, 0.05, 0.08, 0.1], # 0.12, 0.15
-            'regularization_strength': [0.02, 0.05, 0.08, 0.1], # 0.005, 0.01, 0.15, 0.2
-            'epochs': [150, 300, 400] # 500, 750, 1000, 1200, 1500
+            'learning_rate': [0.01, 0.05, 0.1], # 0.01, 0.05, 0.08, 0.1, 0.12, 0.15
+            'regularization_strength': [0.02, 0.05, 0.08, 0.1], # 0.005, 0.01, 0.02, 0.05, 0.08, 0.1, 0.15, 0.2
+            'epochs': [150, 500, 750, 1000, 1500] # 150, 300, 400, 500, 750, 1000, 1200, 1500
         },
         'svm': {
-            'lambda_': [0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.2],
-            'max_iter': [1200, 1500, 2000, 3000], #150, 300, 500, 750, 1000, 4000, 5000
+            'lambda_': [0.01, 0.02, 0.04, 0.06, 0.1, 0.15, 0.2], # 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.2
+            'max_iter': [1500, 2000, 3000, 4000], #150, 300, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000
         },
         'kernel_logistic_regression': {
-            "gamma_values": [0.1, 0.15, 0.2], # 0.12
-            "degree_values": [2], # 3, 4
-            "coef0_values": [1], # 0.5, 1.5
-            "lambda_": [0.005, 0.01], # 0.001
-            "epochs": [150, 300] # 500, 600, 1000
+            "gamma_values": [0.1, 0.15, 0.2], # 0.1, 0.12, 0.15, 0.2
+            "degree_values": [2], # 2, 3, 4
+            "coef0_values": [1], # 0.5, 1, 1.5
+            "lambda_": [0.005, 0.01], # 0.001, 0.005, 0.01
+            "epochs": [300, 500, 1000] # 150, 300, 500, 600, 1000
         },
         'kernel_svm': {
-            "gamma_values": [0.15, 0.2], # 0.1, 0.3
-            "degree_values": [2], # 3, 4
-            "coef0_values": [1], # 0.5, 1.5
-            "lambda_": [0.001, 0.005], # 0.0005, 0.01, 0.05
+            "gamma_values": [0.2, 0.3], # 0.1, 0.15, 0.2, 0.3
+            "degree_values": [2], # 2, 3, 4
+            "coef0_values": [1], # 0.5, 1, 1.5
+            "lambda_": [0.005, 0.01], # 0.0005, 0.001, 0.005, 0.01, 0.05
             "max_iter": [2000, 3000], # 150, 300, 500, 750, 1000, 1500
         }
     }
